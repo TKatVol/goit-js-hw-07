6d19e483a97d8aaf314af7c3532ee2cc06463904
Kateryna Tymoshchuk <katymoschuk@gmail.com>
2021-05-24 04:06:33 +0300

    Tasks - 8

---
+++ js/task-08.js
@@ -0,0 +1,33 @@
+const numberBoxes = document.querySelector('#controls input');
+const createBnt = document.querySelector('[data-action="render"]');
+const destroyBtn = document.querySelector('[data-action="destroy"]');
+const boxes = document.querySelector('#boxes');
+
+createBnt.addEventListener('click', createNewBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createNewBoxes() {
+    const amount = numberBoxes.value;
+    createBoxes(amount);
+};
+
+function createBoxes(amount) {
+    const elements = [];
+  
+    for (let i = 0; i < amount; i += 1) {
+        const minWidth = 30;
+        const minHeigth = 30;
+
+        const box = document.createElement('div');
+        box.style.backgroundColor = '#' + (0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        box.style.width = minWidth + 10 * [i] +'px';
+        box.style.height = minHeigth + 10 * [i] + 'px';
+        elements.push(box) ;
+    }
+  
+   boxes.append(...elements); 
+};
+
+function destroyBoxes() {
+    boxes.innerHTML = '';
+};
